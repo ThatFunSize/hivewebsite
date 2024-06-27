858d240448fffb74da02b5a33c46b68242b43b3d
ThatFunSize <foulofdoom@gmail.com>
2024-06-26 19:52:54 -0600

    working on svg and structuring changes

--- components/banner.tsx
+++ components/banner.tsx
@@ -17,12 +17,8 @@ export default function Banner() {
                 target="_blank"
                 rel="noreferrer"
               >
-                Download<span className="hidden sm:inline"> on GitHub</span>
+                Contact<span className="hidden sm:inline"> Us</span>
               </a>{' '}
-              <span className="italic px-1.5">or</span>{' '}
-              <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
-                Check Premium Version
-              </a>
             </div>
             <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
               <span className="sr-only">Close</span>
